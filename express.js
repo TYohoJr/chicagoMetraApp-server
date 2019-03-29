265d7ba5ec7d5e959bbc72a5b2c4d1e118a3300f
Thomas Yoho <TYohoJr@Gmail.com>
2019-03-29 15:32:26 -0500

    added /get-trips and /get-calendar-dates

--- express.js
+++ express.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
     console.log(`${req.body.request_id} Unique????`)
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-}); 
+});
 
 app.listen(process.env.PORT || 8080, () => {
     var curPort = process.env.PORT;
@@ -25,21 +25,43 @@ app.listen(process.env.PORT || 8080, () => {
     }
 })
 
-app.post('/apiTest', (req, res) =>{
-    res.json({
-        hello: 'helllllo'
-    })
-})
-
-app.get("/apiTest", (req, res) => {
+app.get('/get-alerts', (req, res) => {
     request(
         {
-            url: `https://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@gtfsapi.metrarail.com/gtfs/positions`,
+            url: `https://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@gtfsapi.metrarail.com/gtfs/alerts`,
             json: true
         },
         function (error, response, body) {
             res.json({
-                body: body
+                body
+            })
+        }
+    );
+})
+
+app.get('/get-trips', (req, res) => {
+    request(
+        {
+            url: `https://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@gtfsapi.metrarail.com/gtfs/schedule/trips`,
+            json: true
+        },
+        function (error, response, body) {
+            res.json({
+                body
+            })
+        }
+    );
+})
+
+app.get('/get-calendar-dates', (req, res) => {
+    request(
+        {
+            url: `https://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@gtfsapi.metrarail.com/gtfs/schedule/calendar_dates`,
+            json: true
+        },
+        function (error, response, body) {
+            res.json({
+                body
             })
         }
     );
